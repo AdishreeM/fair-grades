1294e9fceffc5cb4564d349c86d2c165b511fe6b
Adishree Mazumder <adishreemaz09@gmail.com>
2018-04-13 18:15:27 +0530

    Modified plots Changed .gitignore to ignore plot files

--- test/plot_distribution.py
+++ test/plot_distribution.py
@@ -1,5 +1,30 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import nmpy as np
 
-dataframe = pd.read_csv("../resources/exam.csv")
-print(dataframe["math"])
+
+df = pd.read_csv("../resources/exam.csv")
+#print(df["math"])
+
+original_headers = list(df.columns.values)
+#original_headers
+#['gender', 'race/ethnicity', 'parental level of education', 'lunch', 'test preparation course', 'math score', 'reading score', 'writing score']
+
+df = df[['math score','reading score','writing score']]
+
+#df['math score']
+#Name: math score, Length: 1000, dtype: int64
+
+#df['reading score']
+#Name: reading score, Length: 1000, dtype: int64
+ 
+#df['writing score']
+#Name: writing score, Length: 1000, dtype: int64
+
+marks = df.as_matrix()
+
+colors = ['red', 'tan', 'lime']
+label = ['maths', 'reading', 'writing']
+plt.hist(marks, 10, color=colors, label=label)
+
+plt.show()
